00c4a1017289cd14cd4fa0649e194d36414711d9
mohitsshetty986 <mohitsshetty986@gmail.com>
2025-11-24 22:19:53 -0500

    Fix the database url and frontend url

--- src/main/java/blog/controller/BlogController.java
+++ src/main/java/blog/controller/BlogController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "http://54.87.136.200:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/blogs")
 public class BlogController {
