ca61f9b819430f918c4320a79373f15e6222142e
mohitsshetty986 <mohitsshetty986@gmail.com>
2026-06-26 02:50:47 -0500

    Fix the import for IOException

--- src/main/java/blog/security/JwtAuthenticationFilter.java
+++ src/main/java/blog/security/JwtAuthenticationFilter.java
@@ -11,6 +11,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 import java.util.Collections;
 
 @Component
